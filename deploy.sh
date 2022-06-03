73ca7e542fe1a1cb7cd6902948c1e5bc4387ff7f
santiago <santysjr956@gmail.com>
2022-06-03 13:12:46 -0300

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-
+git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:santy-ramirez/todo-app-v2.git main:gh-pages
+  git push -f git@github.com:santy-ramirez/instagram-clon.git main:gh-pages
 
 cd -
